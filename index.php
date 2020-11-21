71763cd47916f1d6c72d637f72e06003ddb9d235
Olaniyan Elias Akin <elias.akin@gmail.com>
2020-11-21 12:24:46 +0100

    Index Page

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$myFullName = "Olaniyan Elias"; //String
+$NumberOfChildren = 3; //Numeric
+$HairColor = "Black"; //String
+$education = array(); //Array
+
+echo $myFullName . " My color is " . $HairColor;
+
+
+?>
